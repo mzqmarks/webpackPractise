e0ed972451f446a880cfbf76a1c0df2a61cd32da
mzqmarks <331383616@qq.com>
2020-09-24 08:41:29 +0800

    0924

--- lession1/webpack.config.js
+++ lession1/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 module.exports = {
+    mode: 'none',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'output')
+        filename: 'main.js'
     }
 }
