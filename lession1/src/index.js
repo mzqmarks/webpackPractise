ee11798d0ec9416d4f8bb79939e2922adc3fc565
mzqmarks <331383616@qq.com>
2020-09-24 09:49:52 +0800

    20200924_09 file-loader

--- lession1/src/index.js
+++ lession1/src/index.js
@@ -1,5 +1,9 @@
 import createHeading from './heading.js'
 import './main.css'
+import icon from './icon.png'
 const heading = createHeading()
 
 document.body.append(heading)
+const img = new Image()
+img.src = icon
+document.body.append(img)
--- lession1/webpack.config.js
+++ lession1/webpack.config.js
@@ -4,11 +4,13 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path:path.join(__dirname, 'dist')
+        path:path.join(__dirname, 'dist'),
+        publicPath: 'dist/',
     },
     module: {
         rules: [
-            { test: /\.css$/, use:['style-loader', 'css-loader'] }
+            { test: /\.css$/, use:['style-loader', 'css-loader'] },
+            { test: /\.png$/, use: 'file-loader' }
         ]
     }
 }
