e4477365446b0151a979f3daafa2fb208e2f40ba
mzqmarks <331383616@qq.com>
2020-09-25 09:31:04 +0800

    0925 url-loader

--- lession1/webpack.config.js
+++ lession1/webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
     module: {
         rules: [
             { test: /\.css$/, use:['style-loader', 'css-loader'] },
-            { test: /\.png$/, use: 'file-loader' }
+            { 
+                test: /\.png$/,
+                use: {
+                     loader: 'url-loader',
+                     options: {
+                         limit: 10 * 1024 //限制10M以下的图片用url-loader打包，10M以上的还用file-loader 打包，做了限制必须下载file-loader
+                     }
+                 }
+            }
         ]
     }
 }
