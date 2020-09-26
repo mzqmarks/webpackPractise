5b4b1e4acdd157fd24e46987a05cdf5eae646f2a
Michael <331383616@qq.com>
2020-09-26 16:08:14 +0800

    0926html-loader

--- lession1/src/index.js
+++ lession1/src/index.js
@@ -1,5 +1,6 @@
 import createHeading from './heading.js'
 import './main.css'
+import footerHteml from './footer.html'
 import icon from './icon.png'
 const heading = createHeading()
 
@@ -7,3 +8,4 @@ document.body.append(heading)
 const img = new Image()
 img.src = icon
 document.body.append(img)
+document.write(footerHteml)
--- lession1/webpack.config.js
+++ lession1/webpack.config.js
@@ -10,7 +10,76 @@ module.exports = {
     module: {
         rules: [
             { test: /\.css$/, use:['style-loader', 'css-loader'] },
-            { test: /\.png$/, use: 'file-loader' }
+            { test: /\.png$/, use: 'file-loader' },
+            { 
+                test: /\.js$/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: {
+                        list: [
+                            // All default supported tags and attributes
+                            '...',
+                            {
+                              tag: 'img',
+                              attribute: 'data-src',
+                              type: 'src',
+                            },
+                            {
+                              tag: 'img',
+                              attribute: 'data-srcset',
+                              type: 'srcset',
+                            },
+                            {
+                              // Tag name
+                              tag: 'a',
+                              // Attribute name
+                              attribute: 'href',
+                              // Type of processing, can be `src` or `scrset`
+                              type: 'src'
+                              // Allow to filter some attributes
+                             
+                            },
+                          ],
+                    }
+                }
+            }
+            // { 
+            //     test: /\.html$/, 
+            //     use: {
+            //         loader: 'html-loader',
+            //         options: {
+            //             attrs: ['img:src','a:href']
+            //         }
+            //     }
+                
+            // }
         ]
     }
 }
+
+// loader: 'html-loader', 
+// options: {
+//     attributes: {
+//         list: [
+//             {
+//                 tag: 'img',
+//                 attribute: 'src',
+//                 type: 'srcset'
+//             },
+//             {
+//                 tag: 'a',
+//                 attribute: 'data-href',
+//                 type: 'href'
+//             }
+//         ]
+//     }
+// }
